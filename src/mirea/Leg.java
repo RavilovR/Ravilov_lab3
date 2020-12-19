9d08ca58f64b4c25b3d3670555b102f0b297c8fa
yakol <ya.kolysh@gmail.com>
2020-12-19 21:38:49 +0300

    Initial commit

---
+++ src/mirea/Leg.java
@@ -0,0 +1,21 @@
+package mirea;
+
+public class Leg {
+    private int legSize;
+
+    public Leg(int legSize) {
+        this.legSize = legSize;
+    }
+
+    public int getLegSize() {
+        return legSize;
+    }
+
+    public void setLegSize(int legSize) {
+        this.legSize = legSize;
+    }
+
+    void out() {
+        System.out.println("I have " + legSize + " leg size.");
+    }
+}
